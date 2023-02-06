520461471d02e1b73bd53093ba9837b247aa8ebb
Ulyana-AA <andreeva.ulyanakpc572020@gmail.com>
2023-02-06 19:54:28 +0300

    Добавьте файлы проекта.

---
+++ lab2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace lab2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            Console.WriteLine("Введите число для сравнения: ");
+            int digit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину двумерного массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            
+            int[,] mas1 = new int[n, n];
+            int[] mas2 = new int[n];
+            
+            for (int i = 0; i < mas1.GetUpperBound(0) + 1; i++)
+            {
+                Console.WriteLine("Введите элементы строки массива: ");
+                for (int j = 0; j < mas1.GetUpperBound(1) + 1; j++)
+                {
+                    mas1[i, j] = Convert.ToInt32(Console.ReadLine());
+                    if (mas1[i, j] < digit) sum += mas1[i, j];
+                }
+                mas2[i] = sum;
+                sum = 0;
+            }
+
+            for (int i = 0; i < mas2.Length; i++) Console.Write($"{mas2[i]} ");
+   
+        }
+    }
+}
